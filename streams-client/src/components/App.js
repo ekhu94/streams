ede15e1697cfa49e343380bc2f1e64350bfca288
Erik Huang <ekhu94@gmail.com>
2021-04-05 11:36:33 -0700

    create and add Header nav element inside Router

--- streams-client/src/components/App.js
+++ streams-client/src/components/App.js
@@ -5,12 +5,14 @@ import StreamShow from './streams/StreamShow';
 import StreamCreate from './streams/StreamCreate';
 import StreamEdit from './streams/StreamEdit';
 import StreamDelete from './streams/StreamDelete';
+import Header from './Header';
 
 const App = () => {
     return (
         <div>
             <Router>
-                <div>
+                <Header />
+                <div className="ui container">
                     <Route path="/" exact component={StreamList} />
                     <Route path="/streams/show" exact component={StreamShow} />
                     <Route path="/streams/new" exact component={StreamCreate} />
