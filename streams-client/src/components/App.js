e778449ece3c182c52777be3ddf9108cc95e7328
Erik Huang <ekhu94@gmail.com>
2021-04-05 11:15:35 -0700

    set up Routes for all streams URLs

--- streams-client/src/components/App.js
+++ streams-client/src/components/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import StreamList from './streams/StreamList';
+import StreamShow from './streams/StreamShow';
+import StreamCreate from './streams/StreamCreate';
+import StreamEdit from './streams/StreamEdit';
+import StreamDelete from './streams/StreamDelete';
 
 const App = () => {
     return (
         <div>
-            App
+            <Router>
+                <div>
+                    <Route path="/" exact component={StreamList} />
+                    <Route path="/streams/show" exact component={StreamShow} />
+                    <Route path="/streams/new" exact component={StreamCreate} />
+                    <Route path="/streams/edit" exact component={StreamEdit} />
+                    <Route path="/streams/delete" exact component={StreamDelete} />
+                </div>
+            </Router>
         </div>
     );
 };
