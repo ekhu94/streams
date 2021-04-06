63a268b47c669f28552c975b36bd34403a781311
Erik Huang <ekhu94@gmail.com>
2021-04-06 05:41:16 -0700

    onAuthChange listens for change in auth status using isSignedIn.listen()

--- streams-client/src/components/GoogleAuth.js
+++ streams-client/src/components/GoogleAuth.js
@@ -1,20 +1,39 @@
 import React from 'react';
 
 class GoogleAuth extends React.Component {
+    state = { isSignedIn: null };
 
     componentDidMount() {
         window.gapi.load('client:auth2', () => {
             window.gapi.client.init({
                 clientId: '374070928982-7glbpt83f61vnc2hbcmk4om6reff6sdg.apps.googleusercontent.com',
                 scope: 'email'
+            }).then(() => {
+                this.auth = window.gapi.auth2.getAuthInstance();
+                this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+                this.auth.isSignedIn.listen(this.onAuthChange)
             });
         });
+    }
+
+    onAuthChange = () => {
+        this.setState({ isSignedIn: this.auth.isSignedIn.get() })
+    }
+
+    renderAuthButton() {
+        if (this.state.isSignedIn === null) {
+            return 'Not sure';
+        } else if (this.state.isSignedIn) {
+            return 'Signed In';
+        } else {
+            return 'Not Signed In';
+        }
     }
 
     render() {
         return (
             <div>
-                Google Auth
+                {this.renderAuthButton()}
             </div>
         );
     }
